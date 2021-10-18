73c0dd5e7af41df54b2efddd84c4a1876669fe0e
K1ngS <50477395+kingsworm0@users.noreply.github.com>
2021-10-18 16:23:05 +0300

    Create passport .js
    adding up seria;lize data

---
+++ passport .js
@@ -0,0 +1,9 @@
+passport.serializeUser(function(user, done) {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
